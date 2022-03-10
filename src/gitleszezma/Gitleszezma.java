ddea34efd45694bb18026e9ab769b342a12a76b9
DeltazorZz <93091154+DeltazorZz@users.noreply.github.com>
2022-03-10 12:28:50 +0100

    2. név szerkeztese

--- src/gitleszezma/Gitleszezma.java
+++ src/gitleszezma/Gitleszezma.java
@@ -2,7 +2,7 @@ package gitleszezma;
 public class Gitleszezma {
     public static void main(String[] args) {
         String nev1;
-        String nev2;
+        String nev2 = "x.y";
     }
     
 }
