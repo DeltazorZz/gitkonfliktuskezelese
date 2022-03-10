b027081f9e4d6390d8e795841c42519c79c04f3a
SZF1 B <szf1_b@oktatas.local>
2022-03-10 12:29:56 +0100

    1.név inicializalasa

--- src/gitleszezma/Gitleszezma.java
+++ src/gitleszezma/Gitleszezma.java
@@ -1,7 +1,7 @@
 package gitleszezma;
 public class Gitleszezma {
     public static void main(String[] args) {
-        String nev1;
+        String nev1 = "vetesi";
         String nev2;
     }
     
