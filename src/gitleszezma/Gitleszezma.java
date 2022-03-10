f9d6ea1569e4b40af871e8bc5a1faead605e12da
szf1_b <szf1_b@Tanulo-202-1.oktatas.local>
2022-03-10 12:41:04 +0100

    3.név deklaralasa

--- src/gitleszezma/Gitleszezma.java
+++ src/gitleszezma/Gitleszezma.java
@@ -1,9 +1,10 @@
 package gitleszezma;
 public class Gitleszezma {
     public static void main(String[] args) {
+        int i = 7;
         String nev1 = "vetesi";
         String nev2 = "x.y";
-
+        int nev3;
     }
     
 }
